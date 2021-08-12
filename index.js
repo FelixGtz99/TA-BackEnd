a3a2e2056f903bb75f927f39c3bee65f4bfc8fbc
FelixGtz <felix1699@gmail.com>
2021-08-12 11:55:26 -0700

    first Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const { dbConnection } = require("./database/config");
+const app = express();
+//cors
+app.use(cors());
+
+//lectura y parseo del body
+app.use(express.json());
+
+dbConnection();
+
+//Directorio Publico
+
+
+//rutas
+
+app.listen(process.env.PORT, () => {
+  console.log("esta  running el hijo de su puta madre en " + process.env.PORT);
+});
